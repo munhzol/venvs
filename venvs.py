81c974f7accabaaa807e48875def8c3fe341afc0
munhzol <munhzol@gmail.com>
2020-04-19 06:57:19 -0400

    some applescript update

--- venvs.py
+++ venvs.py
@@ -86,10 +86,30 @@ if cmd == 'activate':
             print('- Error: Please check your venvID.')
             quit()
 
-        script=f"""tell application "Terminal"
-                    do script "source {venvs[venvID]['path']}/{venvs[venvID]['name']}/bin/activate"
-                    activate
-                end tell"""
+        script=f"""execCmd("source {venvs[venvID]['path']}/{venvs[venvID]['name']}/bin/activate",1)
+                on execCmd(cmd, pause)
+                    tell application "System Events"
+                        tell application process "Terminal"
+                            set frontmost to true
+                            keystroke cmd
+                            keystroke return
+                        end tell
+                    end tell
+                    delay pause
+                end execCmd"""
+
+        # tell application "Terminal"
+        #             set currentTab to do script "source {venvs[venvID]['path']}/{venvs[venvID]['name']}/bin/activate"
+        #         end tell
+
+        # script=f"""say "You are not listening to me!" using "Bubbles" -- result: spoken in Bubble"""
+
+        # print("source {venvs[venvID]['path']}/{venvs[venvID]['name']}/bin/activate")
+        # script=f"""tell application "Terminal"
+        #         activate
+        #         tell application "System Events" to key code 36 #return
+        #     end tell"""
+        
 
         applescript.run(script)
         print('DONE!')
